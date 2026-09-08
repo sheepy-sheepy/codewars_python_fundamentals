dfc44bfffa570d3fd54290de355ffd43058e5ed8
sheepy-sheepy <tereshchenkopolina0@mail.ru>
2026-09-08 20:25:17 +0300

    best practice

--- count_of_positives_sum_of_negatives.py
+++ count_of_positives_sum_of_negatives.py
@@ -1,7 +1,13 @@
 def count_positives_sum_negatives(arr: list[int]) -> list[int]:
-    count_positives = sum(1 for num in arr if num > 0)
-    sum_negatives = sum(num for num in arr if num < 0)
-    return [count_positives, sum_negatives] if arr else []
+    if not arr:
+        return []
 
+    count_positives = 0
+    sum_negatives = 0
 
-print(count_positives_sum_negatives([]))
+    for num in arr:
+        if num > 0:
+            count_positives += 1
+        elif num < 0:
+            sum_negatives += num
+    return [count_positives, sum_negatives]
