9431a05a32f4350cd9f6ef9fa656ffc0ee43497f
sheepy-sheepy <tereshchenkopolina0@mail.ru>
2026-09-12 23:08:56 +0300

    best practice

--- array_diff.py
+++ array_diff.py
@@ -1,6 +1,2 @@
 def array_diff(a: list[int], b: list[int]) -> list[int]:
-    diff_digits = list(set(a).difference(set(b)))
-    return [digit for digit in a if digit in diff_digits]
-
-
-print(array_diff([1, 2, 2], [1]))
+    return [digit for digit in a if digit not in b]
